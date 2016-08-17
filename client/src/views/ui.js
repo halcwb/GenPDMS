938a1f4b54a07ac8520bcba82d696e1b6f6f1add
Casper Bollen <halcwb@gmail.com>
2016-08-17 14:24:04 +0200

    feat: ui mockup

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -23,7 +23,9 @@
         var header = require('./bars/header.js'),
             menu = require('./menus/sideMenu.js'),
             status = require('./templates/bottomBar.js'),
-            debug = app.debug('views:ui');
+            debug = app.debug('views:ui'),
+
+            labelWidth = 100;
 
 
         debug('init');
@@ -38,29 +40,172 @@
             rows: [
                 header.view(app),
                 {cols: [
-                    {
-                        view: 'datatable',
-                        resizeColumn: true,
-                        select: 'row',
-                        columns: [
-                            { id: 'no', header: 'HospNo', sort: 'string' },
-                            { id: 'name', header: 'Name', fillspace: true, sort: 'string' }
-                        ],
-                        data: [
-                            { id: '1', no: '1', name: 'John Cedar' },
-                            { id: '2', no: '2', name: 'Frederick Maple' },
-                            { id: '3', no: '3', name: 'Christine Damian' },
-                            { id: '4', no: '4', name: 'Eric Underwood' }
-                        ]
-                    },
+                    { rows: [
+                        {
+                            view: 'datatable',
+                            id: 'list_patient',
+                            resizeColumn: true,
+                            select: 'row',
+                            columns: [
+                                { id: 'no', header: 'HospNo', sort: 'string' },
+                                { id: 'name', header: 'Name', fillspace: true, sort: 'string' }
+                            ],
+                            data: [
+                                { id: '1', no: '1', name: 'John Cedar' },
+                                { id: '2', no: '2', name: 'Frederick Maple' },
+                                { id: '3', no: '3', name: 'Christine Damian' },
+                                { id: '4', no: '4', name: 'Eric Underwood' }
+                            ]
+                        }
+                    ]},
                     { view: 'resizer' },
-                    { template: 'details'}
-
+                    { rows: [
+                        {
+                            view: 'form',
+                            id: 'form_patient_details',
+                            elements: [
+                                { rows: [
+                                    { template: 'Patient Details', type: 'section' },
+                                    {
+                                        view: "text",
+                                        label: 'Id',
+                                        placeholder: 'Hospital Number',
+                                        name: 'no',
+                                        labelAlign: 'right',
+                                        labelWidth: labelWidth
+                                    },
+                                    {
+                                        view: "text",
+                                        label: 'Last Name',
+                                        placeholder: 'First Name',
+                                        name: 'fname',
+                                        labelAlign: 'right',
+                                        labelWidth: labelWidth
+                                    },
+                                    {
+                                        view: "text",
+                                        label: 'First Name',
+                                        placeholder: 'Last Name',
+                                        name: 'lname',
+                                        labelAlign: 'right',
+                                        labelWidth: labelWidth
+                                    },
+                                    {
+                                        view: "datepicker",
+                                        label: 'Birth date',
+                                        placeholder: 'Patient Birth Date',
+                                        name: 'dob',
+                                        labelAlign: 'right',
+                                        labelWidth: labelWidth
+                                    },
+                                    { template: '', height: 20 },
+                                    {
+                                        cols: [
+                                            { template: '' },
+                                            { view: 'button', value: 'New', width: 75},
+                                            { view: 'button', value: 'Edit', type: 'form', width: 75},
+                                            { view: 'button', value: 'Save', width: 75 }
+                                        ]
+                                    }
+                                ]}
+                            ]
+                        },
+                        { view: 'resizer' },
+                        {
+                            view: 'tabview',
+                            cells: [
+                                {
+                                    header: 'Indications',
+                                    body: {
+                                        rows: [
+                                            {
+                                                view: 'datatable',
+                                                resizeColumn: true,
+                                                select: 'row',
+                                                editable: true,
+                                                columns: [
+                                                    {
+                                                        id: 'sign',
+                                                        header: 'Signs',
+                                                        fillspace: true,
+                                                        sort: 'string'
+                                                    },
+                                                    {
+                                                        id: 'value',
+                                                        header: 'Value'
+                                                    },
+                                                    {
+                                                        id: 'unit',
+                                                        header: 'Unit'
+                                                    }
+                                                ],
+                                                data: [
+                                                    { id: '1', sign: 'Pain Score', value: '3', unit: '' },
+                                                    { id: '2', sign: 'Mean Blood Pressure', value:  '80', unit: 'mmHg' }
+                                                ]
+                                            },
+                                            {
+                                                view: 'toolbar',
+                                                height: 40,
+                                                cols: [
+                                                    { template: '' },
+                                                    { view: 'button', value: 'Add', width: 75 }
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                },
+                                {
+                                    header: 'Treatment',
+                                    body: {
+                                        rows: [
+                                            {
+                                                view: 'datatable',
+                                                resizeColumn: true,
+                                                select: 'row',
+                                                columns: [
+                                                    { id: 'id', header: 'Id', sort: 'string' },
+                                                    {
+                                                        id: 'order',
+                                                        header: 'Order',
+                                                        fillspace: true,
+                                                        sort: 'string'
+                                                    }
+                                                ],
+                                                data: [
+                                                    { id: '1', order: 'Dopamin' },
+                                                    { id: '2', order: 'Paracetamol' },
+                                                    { id: '3', order: 'Morfine' }
+                                                ]
+                                            },
+                                            {
+                                                view: 'toolbar',
+                                                height: 40,
+                                                cols: [
+                                                    { template: '' },
+                                                    { view: 'button', value: 'Review', width: 75 }
+                                                ]
+                                            }
+                                        ]
+                                    }
+                                }
+                            ]
+                        }
+                    ]}
                 ]},
                 status.view(app)
             ]
         });
 
+
+        webix.ui({
+            view: 'contextmenu',
+            id: 'patient_list_menu',
+            data: [
+                'Add',
+                'Remove'
+            ]
+        }).attachTo($$('list_patient'));
 
         // **** Initialize Views ****
 
