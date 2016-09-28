c74adc4358f18cafa5411d64717882b9027a1d0d
halcwb <halcwb@gmail.com>
2016-09-28 17:05:45 +0200

    refactor: model

--- client/src/models/patient.js
+++ client/src/models/patient.js
@@ -1,4 +1,5 @@
 /**
+ * ## Patient Model
  * @module models/patient
  */
 
@@ -6,12 +7,22 @@
 /*global _ */
 
 (function () {
+
     "use strict";
 
-    var name = "patient",
-        model;
+    //region --- IDENTIFIERS AND NAMES ---
 
-    var create = require("model-component")(name)
+    var name = "patient";
+
+    //endregion
+
+    //region --- ADDITIONAL VARIABLES ---
+
+    //endregion
+
+    //region --- MODEL ---
+
+    var model = require("model-component")(name)
         .attr("id", {type: "string" })
         .attr("no", {type: "string" })
         .attr("lname", {type: "string" })
@@ -23,52 +34,86 @@
         .attr("lengthUnit", {type: "string" })
         .attr("bsa", {type: "number" })
         .attr("age", {type: "string" })
-        .attr("ageUnit", {type: "string" });
+        .attr("ageUnit", {type: "string" })({});
 
-    var init = _.once(function (app) {
-        var debug = app.debug("models:patient"),
-            publish   = _.partial(app.bus.model.publish, debug),
-            subscribe = _.partial(app.bus.controller.subscribe, debug),
-            msg = app.msg;
+    //endregion
 
-        model = create({});
-        model.emitChange = true;
+    //region --- HELPER FUNCTIONS ---
 
-        function publishModel () {
-            publish(msg.patient.update, {
-                patient: model.toJSON()
-            });
-        }
-
-        function setModel (data) {
-            model.emitChange = false;
-            model.set(data);
-            model.emitChange = true;
-        }
-
-        model.cancel = function () {
-            setModel(model.origAttrs);
-            publishModel();
-        };
-
-        model.on("change", function (prop, val, old) {
-            if (model.emitChange && !_.isEqual(val, old)) {
-                publishModel();
-            }
+    function publishModel (publish, msg) {
+        publish(msg, {
+            patient: model.toJSON()
         });
+    }
 
-        subscribe(msg.patient.select, function (data) {
-            setModel(data.patient);
+    function cancelModel() {
+        model.set(model.origAttrs);
+    }
+
+    //endregion
+
+    //region --- SUBSCRIBE ---
+
+    var subscribeController = function (app, debug, publish) {
+        var msg = app.msg,
+            sub = _.partial(app.bus.controller.subscribe, debug),
+            pub = _.partial(publishModel, publish);
+
+        debug("subscribe to controller");
+
+        sub(msg.patient.select, function (data) {
+            model.set(data.patient);
             model.clean();
-
-            publishModel();
+            pub(msg.patient.select);
         });
 
-        return model;
-    });
+        sub(msg.patient.update, function (data) {
+            model.set(data.patient);
+            pub(msg.patient.update);
+        });
 
+        sub("patient.cancel", function () {
+            cancelModel();
+            pub(msg.patient.update);
+        });
+    };
+
+    var subscribeOnce = _.once(subscribeController);
+
+    //endregion
+
+    //region --- INITIALIZE ---
+
+    var init = function (app, debug) {
+        var pub = _.partial(app.bus.model.publish, debug);
+        subscribeOnce(app, debug, pub);
+    };
+
+    //endregion
+
+    //region --- EXPORT ---
+
+    /**
+     * #### Get the model name
+     * @returns {string} Name of the model
+     */
     exports.getName = function () { return name; };
 
-    exports.init = function (app) { return init(app); };
+    /**
+     * #### Initializes the model
+     *
+     * - Create subscriptions for the model
+     * - Add publish handlers to model events
+     *
+     * @param {object} app The application namespace
+     */
+    exports.init = function (app) {
+        var deb = app.debug("models:" + name);
+        deb("init");
+        init(app, deb);
+        return model;
+    };
+
+    //endregion
 
 })();
